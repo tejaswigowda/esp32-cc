a047512cca04f9c51c33adb53da846ac55d5b7d0
Ubuntu <tejaswi@asu.edu>
2019-02-22 03:42:03 +0000

    ac

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+sudo apt update
+
 sudo apt-get install gcc git wget make libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future python-pyparsing
 
 export PATH="~/esp32-cc/xtensa-esp32-elf/bin:$PATH"
